a0b72fafb94ee0ca6b3549cf61765c8181df3357
Ronny De Los Santos <delossantosr835@gmail.com>
2024-07-05 11:41:55 -0400

    API RESTful

---
+++ Api/Api Login/Models/GuardarRegistro.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text.Json;
+namespace Api_Login.Models
+{
+    public class GuardarRegistro
+    {
+        List<Registro> listaRegistros = new List<Registro>();
+        public void guardarRegistro(Registro registro)
+        {
+            if (!File.Exists("C:\\Users\\Ronny\\source\\repos\\Api Login\\Api Login\\Registros.json"))
+            {
+                listaRegistros.Add(registro);
+                string jsonRegistro = JsonSerializer.Serialize(listaRegistros);
+                File.WriteAllText("C:\\Users\\Ronny\\source\\repos\\Api Login\\Api Login\\Registros.json", jsonRegistro);
+            }else
+            {
+                string getJson = File.ReadAllText("C:\\Users\\Ronny\\source\\repos\\Api Login\\Api Login\\Registros.json");
+                List<Registro> lista = JsonSerializer.Deserialize<List<Registro>>(getJson);
+                lista.Add(registro );
+                string jsonRegistro = JsonSerializer.Serialize(lista);
+                File.WriteAllText("C:\\Users\\Ronny\\source\\repos\\Api Login\\Api Login\\Registros.json", jsonRegistro);
+            }
+        }
+    }
+}
---
+++ Api/Api Login/Models/Registro.cs
@@ -0,0 +1,18 @@
+﻿namespace Api_Login.Models
+{
+    public class Registro
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string pass {  get; set; }
+
+        public Registro(string nombre, string apellido, string email, string pass)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.email = email;
+            this.pass = pass;
+        }
+    }
+}
