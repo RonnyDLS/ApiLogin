20682abe2b00b38b4a38993b8163fe478ae7f500
Ronny De Los Santos <delossantosr835@gmail.com>
2024-07-05 11:49:24 -0400

    API RESTful

---
+++ Api Login/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Api_Login.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Login.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        [HttpPost("Registrar")]
+        public IActionResult addRegistro(Registro registro)
+        {
+            GuardarRegistro gRegistro = new GuardarRegistro();
+            gRegistro.guardarRegistro(registro);
+            Console.WriteLine(registro.nombre);
+
+            return Ok("Registrado correctamente");
+        }
+        
+        [HttpGet("ObtenerRegistros")]
+        public List<Registro> getRegistros()
+        {
+            LeerRegistros leerRegistros = new LeerRegistros();
+
+            List<Registro> getReg = leerRegistros.leerRegistros();
+            return getReg;
+        }
+
+    }
+}
